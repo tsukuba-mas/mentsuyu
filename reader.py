cab1a61e17c53958dcc6d437fff4d4256a589130
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-11-22 13:46:11 +0100

    use integer values as index

--- reader.py
+++ reader.py
@@ -4,4 +4,6 @@ def readop(path: str) -> pd.DataFrame:
     return pd.read_csv(path, index_col=0, header=None)
 
 def readbel(path: str) -> pd.DataFrame:
-    return pd.read_csv(path, index_col=0, header=None, dtype=str)
+    df = pd.read_csv(path, index_col=0, header=None, dtype=str)
+    df.index = df.index.astype(int)
+    return df
