8266ea29bd4f644ac001d2d0a40627b3932eb538
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-11-22 14:07:03 +0100

    make the index of columns 0-origin

--- reader.py
+++ reader.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
+def makeDfColumn0origin(raw: pd.DataFrame) -> pd.DataFrame:
+    df = raw.copy(deep=True)
+    df.columns = list(range(df.shape[1]))
+    return df
+
 def readop(path: str) -> pd.DataFrame:
-    return pd.read_csv(path, index_col=0, header=None)
+    df = pd.read_csv(path, index_col=0, header=None)
+    return makeDfColumn0origin(df)
 
 def readbel(path: str) -> pd.DataFrame:
     df = pd.read_csv(path, index_col=0, header=None, dtype=str)
     df.index = df.index.astype(int)
-    return df
+    return makeDfColumn0origin(df)
