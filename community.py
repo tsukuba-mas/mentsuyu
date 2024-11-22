ef8e7d596410be9e7662e8e909803f383abcd24b
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-11-22 14:39:09 +0100

    return correct type of values

--- community.py
+++ community.py
@@ -9,10 +9,10 @@ def getBeliefBasedCommunity(bels: list[str]) -> list[set[int]]:
     bel2ids = {}
     for i, bel in enumerate(bels):
         if bel in bel2ids:
-            bel2ids[bel].add(i)
+            bel2ids[bel].append(i)
         else:
             bel2ids[bel] = [i]
-    results = list(bel2ids.values())
+    results = [set(c) for c in bel2ids.values()]
     results.sort(key=lambda x: len(x), reverse=True)
     return results
 
@@ -31,7 +31,7 @@ def getOpinionBasedCommunity(ops: list[list[float]], eps: float = 0.1) -> list[s
         else:
             coms[clsid] = [i]
     
-    communities = list(coms.values())
+    communities = [set(c) for c in coms.values()]
     communities.sort(key=lambda x: len(x), reverse=True)
     return communities
 
