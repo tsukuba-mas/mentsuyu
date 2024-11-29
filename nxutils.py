347ecc3b6d8a4715835e7e601a7776e48fb0d838
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-11-29 15:08:56 +0100

    represent color as hex numbers

--- nxutils.py
+++ nxutils.py
@@ -1,11 +1,12 @@
 import networkx as nx
 
-def mix(low: tuple[int, int, int], high: tuple[int, int, int], rat: float) -> tuple[int, int, int]:
-    return (
+def mix(low: tuple[int, int, int], high: tuple[int, int, int], rat: float) -> str:
+    mixed = (
         int(low[0] * (1 - rat) + high[0] * rat),
         int(low[1] * (1 - rat) + high[1] * rat),
         int(low[2] * (1 - rat) + high[2] * rat),
     )
+    return f"{mixed[0]:02x}{mixed[1]:02x}{mixed[2]:02x}"
 
 def drawWithOpinions(
     G: nx.DiGraph, opinions: list[float],
