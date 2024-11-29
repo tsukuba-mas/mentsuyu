909c3fa844d936aa74a92b5696fd8fbe9d5b78b9
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-11-29 15:03:08 +0100

    add utilities to draw networkx graphs

---
+++ nxutils.py
@@ -0,0 +1,16 @@
+import networkx as nx
+
+def mix(low: tuple[int, int, int], high: tuple[int, int, int], rat: float) -> tuple[int, int, int]:
+    return (
+        int(low[0] * (1 - rat) + high[0] * rat),
+        int(low[1] * (1 - rat) + high[1] * rat),
+        int(low[2] * (1 - rat) + high[2] * rat),
+    )
+
+def drawWithOpinions(
+    G: nx.DiGraph, opinions: list[float],
+    lowest = (0, 0, 255), highest = (255, 0, 0)
+):
+    colors = [mix(lowest, highest, o) for o in opinions]
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos=pos, node_color=colors)
